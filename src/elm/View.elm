0c95b54f3fc6f8a73d42d979b8dce2af996365c7
Leo jPod <leo.jpod@gmail.com>
2017-07-21 02:07:27 +0200

    bulma-ize the View.elm

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -1,6 +1,8 @@
 module View exposing (view)
 
-import Html exposing (Html, div, h1, p, text)
+import Html exposing (Html, div, h1, p, text, section)
+import Html.Attributes exposing (class)
+import BulmaClasses exposing (bulma)
 import Model
 import Update
 
@@ -8,15 +10,31 @@ import Update
 view : Model.State -> Html Update.Msg
 view model =
     div []
-        [ h1 [] [ text "Apache Cordova with Elm and browserSync" ]
-        , div []
-            [ (case model of
-                Model.Loading ->
-                    p [] [ text "Connecting to Device" ]
+        [ header
+        , section
+            [ class bulma.section.container
+            , class bulma.section.spacing.isLarge
+            ]
+            [ div [ class bulma.feature.container ]
+                [ (case model of
+                    Model.Loading ->
+                        p [] [ text "Connecting to Device" ]
 
-                Model.Ready _ ->
-                    p [] [ text "Device is Ready" ]
-              )
-            , p [] [ text "powered by elm" ]
+                    Model.Ready _ ->
+                        p [] [ text "Device is Ready" ]
+                  )
+                , p [] [ text "powered by elm" ]
+                ]
+            ]
+        ]
+
+
+header : Html msg
+header =
+    section [ class bulma.hero.container, class bulma.hero.color.isPrimary ]
+        [ div [ class bulma.hero.body ]
+            [ div [ class bulma.feature.container ]
+                [ h1 [ class bulma.heading.title ] [ text "elm-toggle" ]
+                ]
             ]
         ]
